3e856a274dd025b752a31f3da5728f6f82395618
riley soloner <rileysoloner@gmail.com>
2022-06-29 09:23:29 -0400

    commit tree d64575c963c9a1bad54bf2a4a60efa411d929192

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function addingEventListener() {
     const input = document.getElementById('button');
 
+input.addEventListener('click', clickAlert);
+
+function clickAlert() {
+    alert('I was clicked!');
+  }
+}
+addingEventListener();
+
+/* const input = document.getElementById('button');
+
 function clickAlert() {
   alert('I was clicked!');
 }
 
-input.addEventListener('click', clickAlert);
-}
+input.addEventListener('click', clickAlert); */
